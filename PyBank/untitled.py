b9ac73b75dc552b8e04d24a63d408e561e1bd965
Patrick Thornquist <psthornquist@gmail.com>
2022-01-02 19:32:04 -0700

    final updates

--- PyBank/untitled.py
+++ PyBank/untitled.py
@@ -1,16 +1,9 @@
 # Title Sections
 
-print("Financial Analysis")
-print("----------------------------")
-
-#Import file and include path
-
 from pathlib import Path
 import csv
 
 csvpath = ("../budget_data.csv")
-
-#Open the CSV file, use next to skip the header titles
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -18,43 +11,52 @@ with open(csvpath, 'r') as csvfile:
     date = []
     profit = []
     profit_change = []
+    list1 = []
+    
+    list1.append("Financial Analysis")
+    list1.append("----------------------------")
 
-    #Separate the 2 columns into their own variables. Column 1 is now "date" and column 2 is "profit"
     for row in csvreader:
         date.append(row[0])
         profit.append(float(row[1]))       
 
-# The total number of months included in the dataset. Use len to find the length of the list, which will give us the total number of months because header was already removed.
+# The total number of months included in the dataset.
 
-    print("Total Months:", len(date))
+    str1 = "Total Months:" + str(len(date))
+    list1.append(str1)
 
-# The net total amount of Profit/Losses over the entire period. Use sum function to get the total for the profit column. Use round to make this a whole number. 
+# The net total amount of Profit/Losses over the entire period.
 
-    print("Total:", round(sum(profit)))
+    str2 = "Total:" + str(round(sum(profit)))
+    list1.append(str2)
 
-# The average of the changes in Profit/Losses over the entire period. To figure out the daily change, take the current day profit and subtract the previous day profit. Range starts at 1 because we don't need the first day subtracting 0. This will then be put into a new list called profit_change. To get the average, find the total of the daily changes and divide them by total number of changes. Use round function to get to 2nd decimal place. 
+# The average of the changes in Profit/Losses over the entire period.
 
     for x in range(1,len(profit)):
         profit_change.append(profit[x] - profit[x-1])
         average_profit_change = sum(profit_change) / len(profit_change)              
                
-    print("Average Change: $", round(average_profit_change,2))
+    str3 = "Average Change: $" + str(round(average_profit_change,2))
+    list1.append(str3)
 
-# The greatest increase in profits (date and amount) over the entire period. Use max function to find the greated profit change in new list. Then use index function to find the original location of the date that aligns with that profit change. Add 1 to get the 2nd month out of the two months used in the equation.
+# The greatest increase in profits (date and amount) over the entire period.
     maximum_profit_change = (max(profit_change))
     maximum_profit_change_date = str(date[profit_change.index(max(profit_change))+1])
     
-    print("Greatest Increase in Profits:",maximum_profit_change_date,"($",round(maximum_profit_change),")")  
-
-# The greatest decrease in losses (date and amount) over the entire period. Same course of action as the greatest increase, but change max to min function.
+    str4 = "Greatest Increase in Profits:" + str(maximum_profit_change_date) + "($" + str(round(maximum_profit_change)) + ")"  
+    list1.append(str4)
+    
+# The greatest decrease in losses (date and amount) over the entire period.
 
     minimum_profit_change = (min(profit_change))
     minimum_profit_change_date = str(date[profit_change.index(min(profit_change))+1])
     
-    print("Greatest Decrease in Profits:",minimum_profit_change_date,"($",round(minimum_profit_change),")")
+    str5 = "Greatest Decrease in Profits:" + str(minimum_profit_change_date)+ "($" + str(round(minimum_profit_change)) + ")"
+    list1.append(str5)
+    
+    output_path = Path("PyBank_output.txt")    
 
- 
-output_path = Path("PyBank_output.txt")    
-
-with open(output_path, 'w') as file:
-    file.write()
+    with open(output_path, 'w') as file:
+        for i in list1:
+            print(i)
+            file.write(i + "\n")
